efd9d4536a301c033e1be1caa35577f1ed745451
Documenter.jl <documenter@juliadocs.github.io>
2025-05-17 15:29:23 +0000

    build based on 481464d

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#IsingLogicMachine","page":"Home","title":"IsingLogicMachine","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for IsingLogicMachine.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"}]
+}
